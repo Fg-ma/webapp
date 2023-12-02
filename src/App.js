2cd2d156939f486b95228db84899f76860c8742b
Fg-ma <fgeqma@gmail.com>
2023-12-01 22:56:15 -0500

    left panel working somewhat

--- src/App.js
+++ src/App.js
@@ -1,13 +1,69 @@
 import React from "react";
 import LeftSpace from "./leftSpace";
 import './app.css';
-import "./scrollbar.css"
-import "./leftSpace.css"
+import "./scrollbar.css";
+
+
+import { useState, useEffect } from 'react';
+
+const CustomVerticalSplitPane = () => {
+  const [isResizing, setIsResizing] = useState(false);
+  const [initialMousePosition, setInitialMousePosition] = useState(0);
+  const [initialPaneHeight, setInitialPaneHeight] = useState(0);
+  const [paneHeight, setPaneHeight] = useState('50%');
+
+  const handleMouseMove = (event) => {
+    if (isResizing) {
+      const containerHeight = document.body.clientHeight;
+      const mouseY = event.clientY;
+      const mouseYDelta = mouseY - initialMousePosition;
+
+      // Adjust the speed by multiplying the delta by a factor (e.g., 2)
+      const sensitivityFactor = 2;
+      const newPaneHeight = `${initialPaneHeight + (mouseYDelta / containerHeight) * 100 * sensitivityFactor}%`;
+      setPaneHeight(newPaneHeight);
+    }
+  };
+
+  const handleMouseUp = () => {
+    setIsResizing(false);
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousemove', handleMouseMove);
+    document.addEventListener('mouseup', handleMouseUp);
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove);
+      document.removeEventListener('mouseup', handleMouseUp);
+    };
+  }, [isResizing]);
+
+  const handleMouseDown = (event) => {
+    setIsResizing(true);
+    setInitialMousePosition(event.clientY);
+    setInitialPaneHeight(parseFloat(paneHeight) || 0);
+  };
+
+  return (
+    <div className="custom-vertical-split-pane">
+      <div className="custom-pane" style={{ height: paneHeight }}>
+        Top Pane Content
+      </div>
+      <div className="custom-resizer" onMouseDown={handleMouseDown}>
+      </div>
+      <div className="custom-pane" style={{ height: `calc(100% - ${paneHeight})` }}>
+        Bottom Pane Content
+      </div>
+    </div>
+  );
+};
 
 const App = () =>{
     return (
         <div id="base" className="h-screen">
             <div id="pageSpace" className="flex space-x-14 mx-12 my-16 h-4/5">
+                
                 <LeftSpace />
 
                 <div id="middleSpace" className="rounded-xl">
@@ -27,7 +83,7 @@ const App = () =>{
                         <p>Hot News/Explore</p>
                     </div>
                     <div id="rightContentSpace">
-                        <p>Hot News/Explore Content</p>
+                        <CustomVerticalSplitPane></CustomVerticalSplitPane>
                     </div>
                 </div>
             </div>
--- src/leftSpace.js
+++ src/leftSpace.js
@@ -1,46 +1,115 @@
 import React from "react";
+import "./leftSpace.css";
+
+import { useState, useEffect } from 'react';
 
 export default function LeftSpace() {
     return (
         <div id="leftSpace" className="rounded-xl overflow-hidden">
-            <nav id="leftSelectionBarSpace" className="block w-full rounded-t-xl h-12 bg-fg-white-90 drop-shadow-md">
-                <div className="flex divide-x-2 divide-fg-white-70 h-full">
-                    <div className="w-1/3 my-auto grid place-items-center">
-                        <button className="text-lg underline-offset-8 underline decoration-fg-primary decoration-2">Individuals</button>
-                    </div>
-                    <div className="w-1/3 my-auto grid place-items-center">
-                        <button className="text-lg">Groups</button>
-                    </div>
-                    <div className="w-1/3 my-auto grid place-items-center">
-                        <button className="text-lg">Organizations</button>
-                    </div>
-                </div>
-            </nav>
-            <div id="leftSpaceContent" className="flex-col h-full">
-                <div id="individualCardsContainer" className="overflow-auto resize-y max-h-full h-2/3 mr-3">
-                    <IndividualCards />
-                </div>
-                <div className="h-1/3">
-                    <div id="individualRecHeader" className="h-9 bg-fg-white-90 flex drop-shadow-md">
-                        <div className="grow my-auto grid place-items-start ml-5 text-lg pt-1">
-                            <p>Recommendations...</p>
-                        </div>
-                        <div className="grow my-auto grid place-items-end mr-5">
-                            <button className="bg-fg-black-20 h-2 w-10 rounded"></button>
-                        </div>
-                    </div>
-                    <div id="individualRecContent" className="mr-3 h-full">
-                        <IndividualRecs />
-                    </div>
-                </div>
+            <LeftSpaceNav />
+            <div id="leftSpaceContentContainer" style={{ height: `calc(100% - 3rem)` }}>
+                <CustomVerticalSplitPane />
             </div>
         </div>
     )
 }
 
+function CustomVerticalSplitPane() {
+    const [isResizing, setIsResizing] = useState(false);
+    const [initialMousePosition, setInitialMousePosition] = useState(0);
+    const [initialPaneHeight, setInitialPaneHeight] = useState(0);
+    const [paneHeight, setPaneHeight] = useState('60%');
+
+    const handleMouseMove = (event) => {
+        if (isResizing) {
+            const containerHeight = document.getElementById("leftSpaceContentContainer").offsetHeight;
+            const mouseY = event.clientY;
+            const mouseYDelta = mouseY - initialMousePosition;
+
+            // Adjust the speed by fiddling with the sensetivity factor
+            const sensitivityFactor = 1;
+            let newPaneHeight = initialPaneHeight + (mouseYDelta / containerHeight) * 100 * sensitivityFactor;
+
+            // Cap the newPaneHeight to a maximum value
+            const maxPaneHeight = 100;
+            newPaneHeight = Math.min(newPaneHeight, maxPaneHeight);
+
+            const minPaneHeight = 15;
+            newPaneHeight = Math.max(newPaneHeight, minPaneHeight);
+
+            setPaneHeight(`${newPaneHeight}%`);
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsResizing(false);
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [isResizing]);
+
+    const handleMouseDown = (event) => {
+        setIsResizing(true);
+        setInitialMousePosition(event.clientY);
+        setInitialPaneHeight(parseFloat(paneHeight) || 0);
+    };
+
+    return (
+        <div className="vertical-split-pane">
+            <div className="pane" style={{ height: paneHeight }}>
+                <IndividualCards />
+            </div>
+            <div className="resizer" onMouseDown={handleMouseDown}>
+                <IndividualRecHeader />
+            </div>
+            <div className="pane" style={{ height: `calc(100% - ${paneHeight} - 2.25rem)` }}>
+                <IndividualRecs />
+            </div>
+        </div>
+  );
+};
+
+function IndividualRecHeader () {
+    return (
+        <div id="individualRecHeader" className="h-9 bg-fg-white-90 flex drop-shadow-md">
+            <div className="grow my-auto grid place-items-start ml-5 text-lg pt-1">
+                <p>Recommendations...</p>
+            </div>
+            <div className="grow my-auto grid place-items-end mr-5">
+                <button className="bg-fg-black-20 h-2 w-10 rounded"></button>
+            </div>
+        </div>
+    )
+}
+
+function LeftSpaceNav() {
+    return (
+        <nav id="leftSelectionBarSpace" className="block w-full rounded-t-xl h-12 bg-fg-white-90 drop-shadow-md">
+            <div className="flex divide-x-2 divide-fg-white-70 h-full">
+                <div className="w-1/3 my-auto grid place-items-center">
+                    <button className="text-lg underline-offset-8 underline decoration-fg-primary decoration-2">Individuals</button>
+                </div>
+                <div className="w-1/3 my-auto grid place-items-center">
+                    <button className="text-lg">Groups</button>
+                </div>
+                <div className="w-1/3 my-auto grid place-items-center">
+                    <button className="text-lg">Organizations</button>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
 function IndividualRecs() {
     return (
-        <div id="individualRecs" className="overflow-scroll">
+        <div id="individualRecs" className="mr-3 h-full overflow-scroll">
             <div class="individualRecCard" className="bg-white w-fill my-4 mx-6 h-20 flex items-center rounded-md">
                 <div class="individualRecFilePic" className="w-14 h-14 bg-fg-white-85 ml-4 mr-6 rounded-full grid place-items-center">
                     <p>pic</p>
@@ -119,7 +188,7 @@ function IndividualRecs() {
 
 function IndividualCards() {
     return (
-        <div id="individualCards" className="h-full overflow-auto">
+        <div id="individualCards" className="h-full mr-3 overflow-scroll">
             <div class="individualCard" className="bg-white w-fill my-4 mx-6 h-20 flex items-center rounded-md">
                 <div class="individualFilePic" className="w-14 h-14 bg-fg-white-85 ml-4 mr-6 rounded-full grid place-items-center">
                     <p>pic</p>
