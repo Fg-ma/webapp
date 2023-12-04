5874e1e4d3ae5e7ad43c176eb19754192b58e0ca
Fg-ma <fgeqma@gmail.com>
2023-12-04 17:07:46 -0500

    mute

--- src/middle/middleSpace.js
+++ src/middle/middleSpace.js
@@ -3,13 +3,14 @@ import "./middleSpace.css";
 
 export default function MiddleSpace() {
     return (
-        <div id="middleSpace" className="rounded-xl overflow-hidden h-4/5">
-            <CustomVerticalSplitPane />
+        <div id="middleSpace" className="rounded-xl moverflow-hidden h-4/5">
+            <MiddleDrop />
+            <MiddleVerticalSplitPane />
         </div>
     )
 }
 
-function CustomVerticalSplitPane() {
+function MiddleVerticalSplitPane() {
     const [isResizing, setIsResizing] = useState(false);
     const [initialMousePosition, setInitialMousePosition] = useState(0);
     const [initialPaneHeight, setInitialPaneHeight] = useState(0);
@@ -98,4 +99,81 @@ function RelatedIssues() {
             </div>
         </div>
     )
+}
+
+function MiddleDrop() {
+    const [dropped, setDropped] = useState(false)
+
+    function handleDrop() {
+        setDropped(!dropped)
+    }
+
+    const getDropIcon = () => {
+        if (dropped) {
+            return 'dropUp.svg';
+        } else {
+            return 'dropDown.svg';
+        }
+    };
+
+    return (
+        <div id="dropContainer" className="flex flex-col justify-center items-center">
+            <button className="relative h-24 aspect-square bg-cover bg-no-repeat" style={{ backgroundImage: `url("assets/icons/${getDropIcon()}")` }} onClick={handleDrop}></button>
+            {dropped ? <MiddleSearchBar /> : null}
+        </div>
+    )
+}
+
+
+function MiddleSearchBar() {
+    const [isInputFocused, setIsInputFocused] = useState(false);
+    const [inputValue, setInputValue] = useState('');
+  
+    const handleInputFocus = () => {
+        setIsInputFocused(true);
+    };
+  
+    const handleInputBlur = () => {
+        setIsInputFocused(false);
+    };
+  
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
+  
+    const getSearchIcon = () => {
+        if (isInputFocused || inputValue.trim() !== '') {
+            return 'searchArrow.svg';
+        } else {
+            return 'search.svg';
+        }
+    };
+  
+    return (
+        <form className="w-4/5 h-10 bg-fg-white-85 rounded-md overflow-clip flex items-center">
+            <input
+                id="middleSearchSubmit"
+                type="submit"
+                value=""
+                className="w-6 h-6 bg-cover bg-no-repeat ml-2"
+                style={{ backgroundImage: `url("assets/icons/${getSearchIcon()}")` }}
+            />
+            <input
+                id="middleSearchArea"
+                type="text"
+                placeholder="Search..."
+                className="grow h-full outline-none bg-fg-white-85 placeholder-fg-black-25 text-lg mx-1"
+                onFocus={handleInputFocus}
+                onBlur={handleInputBlur}
+                onChange={handleInputChange}
+                value={inputValue}
+            />
+            <input
+                id="middleFilterButton"
+                type="button"
+                className="w-8 h-8 bg-cover bg-no-repeat mr-2"
+                style={{ backgroundImage: `url("assets/icons/filter.svg")` }}
+            />
+        </form>
+    );
 }
--- src/right/RightSearchBar.js
+++ src/right/RightSearchBar.js
@@ -5,36 +5,36 @@ export default function RightSearchBar() {
     const [inputValue, setInputValue] = useState('');
   
     const handleInputFocus = () => {
-      setIsInputFocused(true);
+        setIsInputFocused(true);
     };
   
     const handleInputBlur = () => {
-      setIsInputFocused(false);
+        setIsInputFocused(false);
     };
   
     const handleInputChange = (event) => {
-      setInputValue(event.target.value);
+        setInputValue(event.target.value);
     };
   
     const getSearchIcon = () => {
-      if (isInputFocused || inputValue.trim() !== '') {
-        return 'searchArrow.svg';
-      } else {
-        return 'searchIcon.svg';
-      }
+        if (isInputFocused || inputValue.trim() !== '') {
+            return 'searchArrow.svg';
+        } else {
+            return 'search.svg';
+        }
     };
   
     return (
-      <div id="rightSpaceSearchBar" className="h-16 w-full bg-fg-white-90 flex justify-center items-center">
-        <form className="w-4/5 h-10 bg-fg-white-85 rounded-md overflow-clip flex items-center">
-              <input
-                  id="rightSearchSubmit"
-                  type="submit"
-                  value=""
-                  className="w-6 h-6 bg-cover bg-no-repeat ml-2"
-                  style={{ backgroundImage: `url("assets/icons/${getSearchIcon()}")` }}
-              />
-              <input
+        <div id="rightSpaceSearchBar" className="h-16 w-full bg-fg-white-90 flex justify-center items-center">
+            <form className="w-4/5 h-10 bg-fg-white-85 rounded-md overflow-clip flex items-center">
+                <input
+                    id="rightSearchSubmit"
+                    type="submit"
+                    value=""
+                    className="w-6 h-6 bg-cover bg-no-repeat ml-2"
+                    style={{ backgroundImage: `url("assets/icons/${getSearchIcon()}")` }}
+                />
+                <input
                     id="rightSearchArea"
                     type="text"
                     placeholder="Search..."
@@ -43,14 +43,14 @@ export default function RightSearchBar() {
                     onBlur={handleInputBlur}
                     onChange={handleInputChange}
                     value={inputValue}
-              />
-              <input
+                />
+                <input
                     id="rightFilterButton"
                     type="button"
                     className="w-8 h-8 bg-cover bg-no-repeat mr-2"
                     style={{ backgroundImage: `url("assets/icons/filter.svg")` }}
-              />
-        </form>
-      </div>
+                />
+            </form>
+        </div>
     );
 }
