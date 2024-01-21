99e341501e1ae97ad7019777322b99925068eefd
Fg-ma <fgeqma@gmail.com>
2024-01-20 22:07:56 -0500

    mute

--- client/src/middle/pages/contentPage/ContentPage.jsx
+++ client/src/middle/pages/contentPage/ContentPage.jsx
@@ -7,10 +7,8 @@ export default function ContentPage() {
     const sheet_id = useSelector((state) => state.page.main.pagePayload.ids.sheet_id);
 
     return (
-        <div className="mr-3 overflow-y-auto" style={{ height: `calc(100% - 2.5rem)` }}>
+        <div className="h-full mr-3 overflow-y-auto">
             <div className="ml-8 mr-5 my-8">
-                <div className='h-10 mb-8 bg-fg-white-90'>
-                </div>
                 <PDFViewer sheet_id={sheet_id} />
             </div>
         </div>
