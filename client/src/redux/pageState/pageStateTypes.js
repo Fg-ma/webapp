2eb181416f77707eb1ea99a3c366e79540e0417e
Fg-ma <134872397+Fg-ma@users.noreply.github.com>
2024-01-21 21:42:50 -0500

    Delete client directory

--- client/src/redux/pageState/pageStateTypes.js
+++
@@ -1,2 +0,0 @@
-export const SET_PAGE_STATE = "SET_PAGE_STATE";
-export const SET_IDS = "SET_IDS";
--- client/src/redux/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import pageStateReducer from './pageState/pageStateReducer';
-import filterReducer from './filters/filterReducer';
-
-export const rootReducer = combineReducers({
-  page: pageStateReducer,
-  filters: filterReducer,
-})
